c897bde34ea6c9aa1247002735bfa1598e5bee16
Steven Childrey <steven.childrey36@yahoo.com>
2018-12-10 23:33:01 -0500

    Password hash and login

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,13 +1,65 @@
+const bcrypt = require('bcrypt');
 const { User } = require('../models');
 
 module.exports = {
   async register(req, res) {
     try {
-      const user = await User.create(req.body);
-      res.send(user.toJSON());
+      const plainTextPW = req.body.password;
+      bcrypt.hash(plainTextPW, 10, (err, hash) => {
+        if (err) {
+          res.status(500).send({
+            error: 'There was a problem contacting the server',
+          });
+        } else {
+          const user = {
+            email: req.body.email,
+            username: req.body.username,
+            password: hash,
+          };
+          User.create(user);
+          res.send(user);
+        }
+      });
     } catch (err) {
       res.status(400).send({
         error: 'This email or username is already in use.',
+      });
+    }
+  },
+  async login(req, res) {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({
+        where: {
+          email,
+        },
+      });
+      if (!user) {
+        res.status(403).send({
+          error: 'The login information was incorrect',
+        });
+      }
+      bcrypt.compare(password, user.password, (err, isValid) => {
+        console.log(isValid);
+        if (err) {
+          res.status(500).send({
+            error: 'There was a problem contacting the server',
+          });
+        } if (isValid) {
+          const userJson = user.toJSON();
+          console.log('made it');
+          res.send({
+            user: userJson,
+          });
+        } else {
+          res.status(403).send({
+            error: 'The login information was incorrect',
+          });
+        }
+      });
+    } catch (err) {
+      res.status(500).send({
+        error: 'An error has occured',
       });
     }
   },
